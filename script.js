aee8fa29e2ff8a5ad89f4299ac0911bb1ccd137c
LPMartinez <lpmartinez1109@gmail.com>
2020-01-02 15:54:41 -0600

    added more fucntions, api key is still an issue

--- script.js
+++ script.js
@@ -1,56 +1,111 @@
-// var APIKey = "166a433c57516f51dfab1f7edaed8413";
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Bujumbura,Burundi&units=imperial&appid=" + APIKey;
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
+
+
+
+
+// function testClick (){
+    //     alert("Click work");
+    //     preventDefault();
+// }    
+
+function currentConditions(){
+    
+}
+function fiveForecast(){
+
+}
+function searchHistory(){
+    var city = $(this).attr("data-city")
+    var APIkey = '27de6d576f342169797bf1826864e56a';
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=27de6d576f342169797bf1826864e56a";    
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(queryURL);
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".temp").text("Temperature (F) " + response.main.temp);s
+        
+    });
+searchHistory();
+}
+
+function uvIndex(){
+
+}
+
+// function getSearchMethod(searchTerm){
+//     if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)    
+//     searchMethod = 'zip';
+//     else
+//         searchMethod = 'q';
+// }
+
+// function searchWeather(searchTerm){
+//     getSearchMethod(searchTerm);    
+//     fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${apikey}&units=${units}`).then(result =>{
+//         return result.json();    
+//     }).then(result =>{
+//         init(result);    
 //     })
-//         .then(function(response){
-//             console.log(queryURL);
-//             console.log(response);
-//             $(".city").html("<h1>" + response.name + "Weather Details</h1>");
-//             $(".wind").html("Wind Speed: " + response.wind.speed);
-//             $(".humidity").text("Humidity: " + response.main.humidity);
-//             $(".temp").text("Temperature (F) " + response.main.temp);
-//             var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-//             $(".tempF").text("Temperature (Kelvin) " + tempF);
-//             console.log("Wind Speed: " + response.wind.speed);
-//             console.log("Humidity: " = response.main.humidity);
-//             console.log("Temperature (F): " + response.main.temp);
+// }
+
+// function init(resultFromServer){
+//     switch (resultFromServer.weather[0].main) {
+//         case 'Clear':    
             
-            
-            
-            
-//         });
+//             break;
+//         case 'Clouds':
+
+//             break;
+//         case 'Rain':
+//         case 'Drizzle':
+//         case 'Mist':
+//             break;
+//         case 'Thuderstorm':
+//             break;
+//         case 'Snow':
+//             break;
+    
+//         default:
+//             break;
+//     }
+//     var weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+//     var temperatureElement = document.getElementById('temperature');
+//     var humidityElement = document.getElementById('humidity');
+//     var windSpeedElement = document.getElementById('windSpeed');
+//     var cityHeader = document.getElementById('cityHeader')
+//     var weatherIcon = document.getElementById('documentIcon');
+
+//     weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + ".png";
+
+//     var resultDescription = resultFromServer.weather[0].description;
+//     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+//     temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
+//     windSpeedElement.innerHTML = 'Winds at' + Math.floor(resultFromServer.wind.speed) + 'm/s';
+//     cityHeader.innerHTML = resultFromServer.name;
+//     humidityElement.innerHTML = 'Humidity levels at' + resultFromServer,main.humidity + '%';
+
+// setPositionForWeatherInfo();
+// }
+
+// function setPositionForWeatherInfo(){
+//     var weatherContainer = document.getElementById('weatherContainer');    
+//     var weatherContainerHeight = weatherContainer.clientHeight;
+//     var weatherContainerWidth = weatherContainer.clientWidth;
+
+//     weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2})`;
+//     weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3})`;
+//     weatherContainer.style.visibility = 'visible';
+// }
+
+// document.getElementById('searchBtn').addEventListener('click', ()=> {
+//     var searchTerm = document.getElementById('searchInput').nodeValue;    
+//     if(searchTerm)
+//         searchWeather(searchTerm);
+// })
 
 
-     
-    //   function displayCityInfo() {
-
-    //     var weather = $(this).attr("data-name");
-    //     var queryURL = "https://openweathermap.org/api" + weather + "&apikey=27de6d576f342169797bf1826864e56a";
-
-       
-    //     $.ajax({
-    //       url: queryURL,
-    //       method: "GET"
-    //     }).then(function(response) {
-    //         console.log(queryURL);
-            
-    //         var nameDiv = $("<div class='city-name'>");
-    //         var name = response.name;
-    //         var pOne = $("<p>").text(name);
-    //         nameDiv.append(pOne);
-    //         $("#city-view").prepend(nameDiv);
-    //     });
-    //     $(document).on("click", ".cityBtn", displayCityInfo);
-    //     console.log(displayCityInfo);
-        
-    //   };
-    function buttonClick(){
-        alert("This button works!")
-        
-    }
-    // document.getElementById("button").addEventListener("click", function(){
-    //     console.log(function buttonClick);
-        
-    // });
